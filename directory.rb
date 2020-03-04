ac981462aea06b58b033c4e4ea2f2df351217364
Faye <Fayecarter333@gmail.com>
2020-03-04 23:27:09 +0000

    Refactoring

--- directory.rb
+++ directory.rb
@@ -1,51 +1,40 @@
-# Define methods
+@students = []
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  i = 0
-  until i == students.length
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)."
-    i += 1
+def print_student_list
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)."
   end
 end
 
-def print_footer(names)
-  if names.count > 1
-    puts "Overall, we have #{names.count} great students"
+def print_footer
+  if @students.count > 1
+    puts "Overall, we have #{@students.count} great students"
   else
-    puts "Overall, we have #{names.count} great student"
+    puts "Overall, we have #{@students.count} great student"
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.delete("\n")
   while !name.empty? do
     puts "what cohort is #{name}?"
     cohort = gets.chomp.to_sym
     cohort = :march if cohort.empty?
-    puts "How old is #{name}?"
-    age = gets.chomp
-    age = "N/A" if age.empty?
-    puts "What hobby does #{name} have?"
-    hobby = gets.chomp
-    hobby = "N/A" if hobby.empty?
-    students << {name: name, cohort: cohort, hobby: hobby, age: age}
-    if students.length > 1
-      puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort}
+    if @students.length > 1
+      puts "Now we have #{@students.count} students"
     else
-      puts "Now we have #{students.count} student"
+      puts "Now we have #{@students.count} student"
     end
-    # get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
 
 def print_starts_with(students)
@@ -76,29 +65,39 @@ def students_by_cohort(students)
   end
 end
 
+def show_students
+  if @students.count > 0
+    print_header
+    print_student_list
+    print_footer
+  else
+    print_footer
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "Try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection 
-      when "1"
-        students = input_students
-      when "2"
-        if students.count > 0
-          print_header
-          print(students)
-          print_footer(students)
-        else
-          print_footer(students)
-        end
-      when "9"
-        exit
-      else
-        puts "Try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
