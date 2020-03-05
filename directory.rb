f3416a2d86a434556b1e8e5bc1cf16302cd8e69d
Faye <Fayecarter333@gmail.com>
2020-03-05 14:20:32 +0000

    Method created to save students to CSV + .gitignore file added

--- directory.rb
+++ directory.rb
@@ -75,12 +75,24 @@ def show_students
   end
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def process(selection)
   case selection
   when "1"
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -91,6 +103,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -100,5 +113,7 @@ def interactive_menu
     process(gets.chomp)
   end
 end
+
+
 
 interactive_menu
