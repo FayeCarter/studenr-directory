2ae39d0a6784fa47213c3bd6420acec436943caa
Faye <Fayecarter333@gmail.com>
2020-03-05 16:16:30 +0000

    Source Code Reader updated to allow user to pick file

--- print_source_code.rb
+++ print_source_code.rb
@@ -1,7 +1,37 @@
-puts "Contents of  #{__FILE__}: \n\n"
+@directory = []
 
-File.open(__FILE__, 'r') do |file|
-  file.readlines.each do |i|
-    puts i
+def get_files
+  Dir.children(__dir__).each_with_index do |file, index|
+    puts "#{index}. #{file}"
+    @directory << {index: index, file: file}
   end
 end
+
+def pick_file
+  chosen_file = ""
+  puts "Which file do you want to read? (enter number)"
+  choice = gets.chomp.to_i
+  @directory.each do |object|
+    if object[:index] == choice
+      chosen_file =  object[:file]
+    end
+  end
+  print_source_code(chosen_file)
+end
+
+def print_source_code(filename)
+  puts "Contents of #{filename}: \n\n"
+  
+  File.open(filename, 'r') do |file|
+    file.readlines.each do |i|
+      puts i
+    end
+  end
+end
+
+def run
+ get_files
+ pick_file
+end
+
+run
